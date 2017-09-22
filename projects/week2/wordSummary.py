9636a00e5589b9741fb93152e215b812804c9e94
Christopher Fudala <cfudala82@me.com>
2017-09-21 22:08:03 -0500

    changes to links for index

--- projects/week2/wordSummary.py
+++ projects/week2/wordSummary.py
@@ -2,3 +2,17 @@ print('Hello world')
 print('practice git')
 print("""what is this all about, will this text editor actually wrap arounor
     not,lets find out""")
+
+def darkmouse(x,y):
+    return
+
+if x == y:
+    print('statement')
+else:
+    x = False
+
+while x < 10:
+    count += 1
+
+while x === 0:
+    return False
