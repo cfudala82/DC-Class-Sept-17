985a9ed67642d202deb788cfa1c318fff4e1fbc8
Christopher Fudala <cfudala82@me.com>
2017-10-03 09:07:20 -0500

    changed pythonObjectExercises

--- projects/week2/pythonObjectExercises.py
+++ projects/week2/pythonObjectExercises.py
@@ -1,4 +1,5 @@
 class Person:
+    """Instantiate's name, email & phone"""
     def __init__(self, name, email, phone):
         self.name = name
         self.email = email
@@ -6,3 +7,20 @@ class Person:
 
     def greet(self, other_person):
         print('Hello {}, I am {}!'.format(other_person.name, self.name))
+
+sonny.Person('Sonny', 'sonny@hotmail.com', '483-485-4948')
+
+# Instantiate an instance object of Person with name of 'Sonny', email of
+# 'sonny@hotmail.com', and phone of '483-485-4948', store it in the variable
+# sonny.
+
+# Instantiate another person with the name of 'Jordan', email of
+# 'jordan@aol.com', and phone of '495-586-3456', store it in the variable 'jordan'.
+
+# Have sonny greet jordan using the greet method.
+
+# Have jordan greet sonny using the greet method.
+
+# Write a print statement to print the contact info (email and phone) of Sonny.
+
+# Write another print statement to print the contact info of Jordan.
