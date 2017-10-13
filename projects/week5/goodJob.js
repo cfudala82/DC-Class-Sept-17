6c24492efa454abe1116972d6db2ec61f56ea0f6
Christopher Fudala <cfudala82@me.com>
2017-10-13 17:37:52 -0500

    changed goodJob.js

---
+++ projects/week5/goodJob.js
@@ -0,0 +1,23 @@
+// Print's out 'Good Job, {{name}}!' for each person's name.
+
+var people = [
+  'Dom',
+  'Lyn',
+  'Kirk',
+  'Autumn',
+  'Trista',
+  'Jesslyn',
+  'Kevin',
+  'John',
+  'Eli',
+  'Juan',
+  'Robert',
+  'Keyur',
+  'Jason',
+  'Che',
+  'Ben'
+];
+
+people.forEach(function(item) {
+  console.log('Good job, ' + item);
+});
