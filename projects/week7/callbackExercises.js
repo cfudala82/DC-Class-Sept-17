39b59b997d9510141232971b3992eae96cd4cab6
Christopher Fudala <cfudala82@me.com>
2017-11-01 18:04:24 -0500

    added callbackExercises.js

---
+++ projects/week7/callbackExercises.js
@@ -0,0 +1,34 @@
+function add(x, y, callback) {
+  setTimeout(function () {
+    var result = x + y;
+    callback(result);
+  }, 1000);
+}
+
+function subtract(x, y, callback) {
+  setTimeout(function () {
+    callback(x - y);
+  }, 1500);
+}
+
+function greeting(person, callback) {
+  setTimeout(function () {
+    callback('Hola, ' + person + '!');
+  }, 2000);
+}
+
+function product(numbers, callback) {
+  setTimeout(function () {
+    var x = numbers.reduce(function(a, b) {
+      return a * b;
+    }, 1);
+    callback(x);
+  }, 2500);
+}
+
+add(1, 2, function (result) { console.log(result); });
+subtract(10, 3, function (result) { console.log(result); });
+greeting('Chris', function (result) { console.log(result); });
+
+var nums = [5, 6];
+product(nums, function (result) { console.log(result); });
