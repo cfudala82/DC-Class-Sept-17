10a6ddc1a7d420b1805e3c649c744e3ca06f2e82
Christopher Fudala <cfudala82@me.com>
2017-11-11 18:35:23 -0600

    changed create_album.js

--- projects/week8/create_album.js
+++ projects/week8/create_album.js
@@ -1,13 +1,46 @@
-var pgp = require('pg-promise')({
-  // initialization options
+
+var prompt = require('prompt-promise');
+var res = [];
+
+prompt('Album name? ')
+.then(function albumName(val) {
+  res.push(val);
+  return prompt('albumYear: ');
+})
+.then(function albumYear(val){
+  res.push(val);
+  return prompt('Artist ID? ');
+})
+.then(function artistId(val) {
+  res.push(val);
+  console.log(res);
+  prompt.done();
+})
+.catch(function rejected(err) {
+  console.log('error:', err.stack);
+  prompt.finish();
 });
-var db = pgp({database: 'restaurant'});
 
 
 
-db.query('SELECT * FROM restaurant')
-  .then(function (results)) {
-    results.forEach(function (r) {
-      console.log(r.id, r.name. r.address, r.category);
-    });
-  });
+
+
+
+
+
+
+
+
+// var pgp = require('pg-promise')({
+//   // initialization options
+// });
+// var db = pgp({database: 'restaurant'});
+//
+//
+//
+// db.query('SELECT * FROM restaurant')
+//   .then(function (results)) {
+//     results.forEach(function (r) {
+//       console.log(r.id, r.name. r.address, r.category);
+//     });
+//   });
