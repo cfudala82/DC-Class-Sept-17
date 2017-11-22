5504c8939d0572cb60a6d607e1ca14da872f7c85
Christopher Fudala <cfudala82@me.com>
2017-11-22 10:57:33 -0600

    changed contactapp

--- projects/week10/contactapp/src/App.jsx
+++ projects/week10/contactapp/src/App.jsx
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import {tealA200, grey900} from 'material-ui/styles/colors';
-import MyForm from './MyForm';
+import {tealA200, darkBlack} from 'material-ui/styles/colors';
+import MyForm from './MyForm'
 import './App.css';
-
 
 const theme = getMuiTheme({
   palette: {
-    textColor: grey900
-  },
-  palette: {
-    textColor: grey900
-  },
+    primary1Color: tealA200,
+    textColor: darkBlack,
+    alternateTextColor: darkBlack
+  }
 });
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider muiTheme={theme}>
-      <MyForm />
+        <MyForm/>
       </MuiThemeProvider>
     );
   }
--- projects/week10/contactapp/src/MyForm.jsx
+++ projects/week10/contactapp/src/MyForm.jsx
@@ -1,9 +1,62 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
+import {Card, CardActions, CardTitle, CardText} from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import './MyForm.css';
+
+
 
 class MyForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: '', email: '', phone: '', address: '', city: '', state: '', zip: ''};
+  }
+
+
   render() {
-    return <AppBar title="Moje Kontakty" />
+    return (
+      <div>
+        <AppBar title="My Contact App" />
+          <div className='new-contact'>
+            <div>
+              <TextField floatingLabelText="Your Name"
+                defaultValue={this.state.name}
+                onChange={event => this.update_state(event, 'name')}/>
+            </div>
+            <div>
+              <TextField floatingLabelText="Your Email"
+                defaultValue={this.state.name}
+                onChange={event => this.update_state(event, 'name')}/>
+            </div>
+            <div>
+              <TextField floatingLabelText="Your Phone Number"
+                defaultValue={this.state.name}
+                onChange={event => this.update_state(event, 'name')}/>
+            </div>
+            <div>
+              <TextField floatingLabelText="Your Address"
+                defaultValue={this.state.name}
+                onChange={event => this.update_state(event, 'name')}/>
+            </div>
+            <div>
+              <TextField floatingLabelText="Your City"
+                defaultValue={this.state.name}
+                onChange={event => this.update_state(event, 'name')}/>
+            </div>
+            <div>
+              <TextField floatingLabelText="Your State"
+                defaultValue={this.state.name}
+                onChange={event => this.update_state(event, 'name')}/>
+            </div>
+            <div>
+              <TextField floatingLabelText="Your Zip Code"
+                defaultValue={this.state.name}
+                onChange={event => this.update_state(event, 'name')}/>
+            </div>
+          </div>
+      </div>
+    );
   }
 }
 
